6627d211162f796b2945640106d229bc4b16a550
Jonier Cabrera Bermudez <ing.jcb.dev@gmail.com>
2022-03-30 15:16:49 -0500

    hasta aqui funciona sin el servicio

--- src/components/paginas/Pokemon.js
+++ src/components/paginas/Pokemon.js
@@ -1,6 +1,14 @@
-import React from "react";
+import React, { useState } from "react";
+import { UseFetch } from "../UseFetch";
 
 const pokemon = () => {
+	// const [url, setUrl] = useState("https://pokeapi.co/api/v2/pokemon");
+
+	// const [url, setUrl] = useState("https://age-of-empires-2-api.herokuapp.com/api/v1/units");
+	// const estado = UseFetch(url);
+	// const { cargando, data } = estado;
+	// cargando ? console.log("cargando") : console.log(data.results);
+
 	return <div>Esta es la pagina de Pokemon</div>;
 };
 
